9f9a482ddd0e21e6b2a99f5c9888797f98703f51
Ashleigh Clark <ashleigh.clark@kaleidocode.co.za>
2025-04-10 14:05:15 +0200

    [WIP] Authentication in Program.cs, appsettings.json

---
+++ src/api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Kaleidocode.Gists.API.Extensions;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMicrosoftWebApiAuthentication(this IServiceCollection svc, IConfiguration cfg)
+    {
+        svc
+            .AddAuthentication(defaultScheme: JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(configuration: cfg, configSectionName: "AzureAd", jwtBearerScheme: JwtBearerDefaults.AuthenticationScheme);
+
+        return svc;
+    }
+}
+
--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,20 +1,22 @@
-
 namespace Kaleidocode.Gists.API;
+
+using Kaleidocode.Gists.API.Extensions;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-
+        builder.Services.AddMicrosoftWebApiAuthentication(builder.Configuration);
         builder.Services.AddControllers();
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        var app = builder.Build();
+        WebApplication? app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
@@ -24,6 +26,13 @@ public class Program
         }
 
         app.UseHttpsRedirection();
+
+        if (app.Environment.IsProduction())
+        {
+            app.UseHsts();
+            app.UseCors();
+        }
+
 
         app.UseAuthorization();
 
